251c41ac50c60b410d8227e18cd59cbe471d5fef
mpsteenstrup <mpsteenstrup@gmail.com>
2025-01-29 12:34:38 +0100

    Kryptering med billede
    Co-authored-by: madsananhansen <madsananhansen@users.noreply.github.com>

--- JSfiler/colorFilter.js
+++ JSfiler/colorFilter.js
@@ -15,6 +15,7 @@ function setup(){
 
 }
 
+Hej Mads
 function draw(){
   ownFilter();
   image(img,w,0);
@@ -25,7 +26,7 @@ function ownFilter(){
   for (let i=0;i<w; i += 1){
     for (let j=0;j<h; j += 1){
       let c = img.get(i,j);
-      fill(getPixelValue(0,i,j),getPixelValue(1,i,j),getPixelValue(2,i,j));
+      fill(255-getPixelValue(0,i,j),255-getPixelValue(1,i,j),255-getPixelValue(2,i,j));
       rect(i,j,1,1);
     }
   }
---
+++ JSfiler/kryptering.js
@@ -0,0 +1,61 @@
+let img, keyImg;
+
+function preload() {
+  keyImg = loadImage('billeder/pixels.png'); // Nøgle billede
+  img = loadImage('billeder/tiger.jpeg'); // Original billede
+}
+
+function setup() {
+  createCanvas(4 * img.width, img.height);
+  noStroke();
+  pixelDensity(1);
+  img.loadPixels();
+  keyImg.loadPixels();
+}
+
+function draw() {
+  let encryptedImg = createImage(img.width, img.height);
+  let decryptedImg = createImage(img.width, img.height);
+
+  encryptedImg.loadPixels();
+  decryptedImg.loadPixels();
+
+  for (let i = 0; i < img.width; i++) {
+    for (let j = 0; j < img.height; j++) {
+      let imgColor = img.get(i, j);
+      let keyColor = keyImg.get(i, j);
+
+      let encryptedColor = encryptPixel(imgColor, keyColor);
+      let decryptedColor = decryptPixel(encryptedColor, keyColor);
+
+      encryptedImg.set(i, j, encryptedColor);
+      decryptedImg.set(i, j, decryptedColor);
+    }
+  }
+
+  encryptedImg.updatePixels();
+  decryptedImg.updatePixels();
+
+  image(img, 0, 0); // Original billede
+  image(keyImg,img.width,0)
+  image(encryptedImg, 2*img.width, 0); // Krypteret billede
+  image(decryptedImg, 3* img.width, 0); // Dekrypteret billede
+
+  noLoop();
+}
+
+function encryptPixel(imgColor, keyColor) {
+  return color(
+    (red(imgColor) + red(keyColor)) % 255,
+    (green(imgColor) + green(keyColor)) % 255,
+    (blue(imgColor) + blue(keyColor)) % 255
+  );
+}
+
+function decryptPixel(encryptedColor, keyColor) {
+  return color(
+    (red(encryptedColor) - red(keyColor) + 255) % 255,
+    (green(encryptedColor) - green(keyColor) + 255) % 255,
+    (blue(encryptedColor) - blue(keyColor) + 255) % 255
+  );
+}
